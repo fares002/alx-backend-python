9e701d4f0b78ac2fd12a7ae472a6d71c15085ff2
fares002 <fares@example.com>
2024-07-09 03:19:51 +0000

    done

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Asyncronous Python
+"""
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    takes int return asyncio task
+    """
+    return asyncio.Task(wait_random(max_delay))
